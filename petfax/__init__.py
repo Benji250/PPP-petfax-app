25f3aa1a1dc782edcc4d3976efbb9bb90dff635e
Benji250 <123209910+Benji250@users.noreply.github.com>
2023-09-25 21:36:07 -0700

    added style and worked on the activity

---
+++ petfax/__init__.py
@@ -0,0 +1,30 @@
+from flask import Flask 
+from flask_migrate import Migrate
+
+def create_app(): 
+    app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Lazytitan2016:)@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIOS'] = False
+
+   
+
+    @app.route('/')
+    def index(): 
+        return 'Hello, PetFax!'
+
+    # register pet blueprint 
+    from . import pet
+    app.register_blueprint(pet.bp)
+
+    from . import fact
+    app.register_blueprint(fact.bp)
+
+     
+     
+    from . import models
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
+
+    # return the app 
+    return app
